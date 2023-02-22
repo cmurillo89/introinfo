e6b1765fc86d22f2ebf36c0e1e56cc008b602029
Cristhofer Murillo Gómez <christopherrmg@gmail.com>
2023-02-21 19:31:25 -0600

    Mi primer commit en logica

---
+++ clase1.py
@@ -0,0 +1,17 @@
+# son 4 tipos de variables
+# String 
+
+name = "Christopher"
+last_name = "Murillo"
+
+# Integer
+id = 113990099
+
+# Float 
+cash = 10.5
+
+# Bool 
+
+active = True
+
+print("hola")
